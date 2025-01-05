cb11ee886dd42a8f104477b5ea59c457e5f5d0c4
vipulrane <vipulrane1@gmal.com>
2025-01-05 10:57:08 +0530

    Array List added for city

---
+++ NovJavaBatchDemo/src/com/velocity/ArrayListDemo1.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo1 {
+	public static void main(String[] args) {
+		ArrayList<String> city=new ArrayList<String>();
+		city.add("Pune");
+		city.add("Jalgaon");
+		city.add("Banglore");
+		
+		System.out.println(city);
+	}
+
+}

5cbf0be86a77ac8e4fc611bd5f204808eef302d1
vipulrane <vipulrane1@gmal.com>
2025-01-05 11:10:45 +0530

    Array List added for numbers

---
+++ NovJavaBatchDemo/src/com/velocity/ArrayListDemo2.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo2 {
+	public static void main(String[] args) {
+		ArrayList<Integer> city=new ArrayList<Integer>();
+		city.add(10);
+		city.add(43);
+		city.add(8);
+		
+		System.out.println(city);
+	}
+
+}

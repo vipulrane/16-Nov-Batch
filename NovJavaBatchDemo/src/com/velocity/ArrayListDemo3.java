560e8f613bfe6b7e4c680989bdb90bddbfda1e6a
vipulrane <vipulrane1@gmal.com>
2025-01-07 21:41:12 +0530

    Country arraylist added

---
+++ NovJavaBatchDemo/src/com/velocity/ArrayListDemo3.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo3 {
+	public static void main(String[] args) {
+		ArrayList<String> country=new ArrayList<String>();
+		country.add("India");
+		country.add("Germany");
+		country.add("Netherland");
+		
+		System.out.println(country);
+	}
+
+}
